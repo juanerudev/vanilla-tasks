24fe0aedc99bf2f0fbd117bf012005b203e743a2
Juan Rodriguez <juanestebanrodriguezurrea@gmail.com>
2024-04-10 21:29:26 -0500

    Se agregaron validaciones a la caja de texto y a los radio button para que se tenga que ingresar la información y poder así almacenar la tarea

--- main.js
+++ main.js
@@ -1,13 +1,58 @@
 const tareaInput = document.querySelector("#tareaInput");
 const btnAgregarTarea = document.querySelector("#btn-agregar");
-
+const listaTareas = document.querySelector("#lista")
+const opciones = document.querySelectorAll('input[type="radio"]')
 
 
 
 const agregarTarea = () => {
-    console.log(tareaInput);
-    console.log(tareaInput.value);
+    const tareaTexto = tareaInput.value;
+
+    if (!validarTexto(tareaTexto)) {
+        alert("Debe escribir algo en la tarea...");
+        return false;
+    }
+
+    if (!validarDificultad()) {
+        alert("Debe seleccionar alguna dificultad...");
+        return false;
+    }
+    
+    opciones.forEach(opcion => {
+        if (opcion.checked) {
+            const nuevaTarea = armarElemento(tareaTexto, opcion.id);
+            listaTareas.appendChild(nuevaTarea)
+        }
+    });
 }
 
+const armarElemento = (texto, dificultad) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a")
+
+    a.innerText = texto
+    li.appendChild(a)
+
+    li.classList.add("tarea", `${dificultad}`)
+
+    return li;
+}
+
+const validarTexto = (texto) => {
+    return texto === "" ? false : true
+}
+
+const validarDificultad = () => {
+    let algoSeleccionado = false;
+
+    opciones.forEach(opcion => {
+        if (opcion.checked) {
+            algoSeleccionado = true;
+            return algoSeleccionado;
+        }
+    });
+
+    return algoSeleccionado;
+}
 
 btnAgregarTarea.addEventListener("click", agregarTarea);
